09c1625f5651033a16dee47520adab03529a2b19
YS739 <suk921@gmail.com>
2022-11-22 12:02:00 +0900

    delete some part that we didn't make function of main.js, router.js. to avoid error

--- js/main.js
+++ js/main.js
@@ -1,56 +1,25 @@
-// import { handleAuth, onToggle } from "./pages/auth.js";
-// import { logout } from "loginmain.html"; // 윤숙 - index로 되어 있어서 loginmain으로 바꿈
-// import { changeProfile, onFileChange } from "./pages/profile.js";
-// import { socialLogin } from "./pages/auth.js";
+import { authService } from "./firebase.js";
 import { handleLocation, route } from "./router.js";
-// 윤숙 goToProfile 잠시 제외함!(페이지 이동이 안 돼서)
-// import { authService } from "./firebase.js";
 
-// import {
-//   save_comment,
-//   update_comment,
-//   onEditing,
-//   delete_comment,
-// } from "./pages/feed.js";
-
+// hash url 변경 시 처리
 window.addEventListener("hashchange", handleLocation);
 
-// 윤숙 - 아래 주석 처리된 코드 쓰면 이 부분 지우기
-document.addEventListener("DOMContentLoaded", handleLocation);
+// 첫 랜딩 또는 새로고침 시 처리
+document.addEventListener("DOMContentLoaded", () => {
+  // 로그인 상태 모니터링
+  authService.onAuthStateChanged((user) => {
+    // Firebase 연결되면 화면 표시
+    // user === authService.currentUser 와 같은 값
+    handleLocation(); //authService가 연결 되면 handleLocation 실행
+    if (user) {
+      // 로그인 상태인 경우
+      alert("로그인 성공");
+    } else {
+      // 로그아웃 상태인 경우
+      alert("로그아웃 완료");
+    }
+  });
+});
 
-// 윤숙 - 회원가입이 되면 쓸 코드라 잠시 주석 처리
-// document.addEventListener("DOMContentLoaded", function () {
-//   authService.onAuthStateChanged((user) => {
-//     handleLocation();
-//     const hash = window.location.hash;
-//     if (user) {
-//       if (hash === "") {
-//         alert("로그인 성공");
-//         window.location.replace("#loginmain");
-//       }
-//     } else {
-//       if (hash !== "") {
-//         alert("로그아웃 성공");
-//         window.location.replace("");
-//       }
-//     }
-//   });
-// });
-
-// function closeTabClick() {
-//   window.close();
-// }
-
-// onclick, onchange, onsubmit 이벤트 핸들러 리스트
+// 전역 함수 리스트
 window.route = route;
-window.onToggle = onToggle;
-window.handleAuth = handleAuth;
-window.goToProfile = goToProfile;
-window.socialLogin = socialLogin;
-window.logout = logout;
-window.onFileChange = onFileChange;
-window.changeProfile = changeProfile;
-window.save_comment = save_comment;
-window.update_comment = update_comment;
-window.onEditing = onEditing;
-window.delete_comment = delete_comment;
--- js/router.js
+++ js/router.js
@@ -1,11 +1,8 @@
-// import { authService } from "./firebase.js";
-
 export const route = (event) => {
   event.preventDefault();
   window.location.hash = event.target.hash;
 };
 
-// 윤숙 - 페이지 경로 설정함 / "/"이 auth.html이 아니라 index.html(우리의 메인 페이지)
 const routes = {
   "/": "/index.html",
   post: "/pages/post.html",
@@ -17,57 +14,16 @@ const routes = {
   404: "/pages/404.html",
 };
 
-// 윤숙 - 댓글을 다는 창이 feed.html이니까 feed.js로 수정함
-// import { getCommentList } from "./pages/feed.js";
-
 export const handleLocation = async () => {
-  let path = window.location.hash.replace("#", "");
-  const pathName = window.location.pathname;
+  let path = window.location.hash.replace("#", ""); // ""
 
-  //윤숙 - 아래 코드 쓰면 여기부터 document.get~ 부분까지 지우기
-
+  // "http://example.com/"가 아니라 도메인 뒤에 / 없이 "http://example.com" 으로 나오는 경우
   if (path.length == 0) {
     path = "/";
   }
+  const route = routes[path] || routes[404]; // truthy 하면 route[path], falsy 하면 routes[404]
 
-  const route = routes[path] || routes[404];
   const html = await fetch(route).then((data) => data.text());
+
   document.getElementById("root").innerHTML = html;
 };
-// 윤숙 - 우리는 메인이 index.page니까 아래 코드가 필요하지 않을 것 같아 일단 주석처리
-// Live Server를 index.html에서 오픈할 경우
-// if (pathName === "/index.html") {
-//   window.history.pushState({}, "", "/");
-// }
-
-// if (path.length == 0) {
-//   path = "/";
-// }
-
-// const route = routes[path] || routes[404];
-// const html = await fetch(route).then((data) => data.text());
-// document.getElementById("root").innerHTML = html;
-
-// 윤숙 - 로그인 구현 전까지 주석처리
-// if (path === "loginmain") {
-//   // 로그인한 회원의 프로필사진과 닉네임을 화면에 표시해줌.
-//   document.getElementById("nickname").textContent =
-//     authService.currentUser.displayName ?? "닉네임 없음";
-
-//   document.getElementById("profileImg").src =
-//     authService.currentUser.photoURL ?? "../assets/blankProfile.webp";
-
-//   getCommentList();
-// }
-//   if (path === "profile") {
-//     document.getElementById("profileView").src =
-//       authService.currentUser.photoURL ?? "/assets/blankProfile.webp";
-//     // 윤숙 - 기본 프로필 사진을 정해서 "/assets~ 부분에 넣어야 함"
-//     document.getElementById("profileNickname").placeholder =
-//       authService.currentUser.displayName ?? "닉네임 없음";
-//   }
-// };
-
-// export const goToProfile = () => {
-//   window.location.hash = "#profile";
-// };
