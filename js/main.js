5ab2f409f8c6d6727aed2cc441618b0cd73d4e0d
YS739 <suk921@gmail.com>
2022-11-22 15:58:10 +0900

    signIn problem

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ window.addEventListener("hashchange", handleLocation);
 // 첫 랜딩 또는 새로고침 시 처리
 document.addEventListener("DOMContentLoaded", () => {
   authService.onAuthStateChanged((user) => {
+    handleLocation();
+    const hash = window.location.hash;
     if (user) {
       if (hash === "auth") {
         window.location.replace("#loginmain");
@@ -26,4 +28,3 @@ window.route = route;
 window.onToggle = onToggle;
 window.handleAuth = handleAuth;
 window.socialLogin = socialLogin;
-window.logout = logout;
