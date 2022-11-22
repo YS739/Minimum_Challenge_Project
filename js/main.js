20e1a6ea5d9a0a00cca20c2f96bd50d36367c1f5
YS739 <suk921@gmail.com>
2022-11-22 10:22:06 +0900

    SPA routing done -edit main.js, router.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,44 @@
-import { handleAuth, onToggle } from "./pages/auth.js";
-import { logout } from "index.html";
-import { changeProfile, onFileChange } from "./pages/profile.js";
-import { socialLogin } from "./pages/auth.js";
-import { handleLocation, goToProfile } from "./router.js";
-import { authService } from "./firebase.js";
-import {
-  save_comment,
-  update_comment,
-  onEditing,
-  delete_comment,
-} from "./pages/feed.js";
+// import { handleAuth, onToggle } from "./pages/auth.js";
+// import { logout } from "index.html";
+// import { changeProfile, onFileChange } from "./pages/profile.js";
+// import { socialLogin } from "./pages/auth.js";
+import { handleLocation, route } from "./router.js";
+// 윤숙 goToProfile 잠시 제외함!(페이지 이동이 안 돼서)
+// import { authService } from "./firebase.js";
+// import {
+//   save_comment,
+//   update_comment,
+//   onEditing,
+//   delete_comment,
+// } from "./pages/feed.js";
 
 window.addEventListener("hashchange", handleLocation);
 
-document.addEventListener("DOMContentLoaded", function () {
-  authService.onAuthStateChanged((user) => {
-    handleLocation();
-    const hash = window.location.hash;
-    if (user) {
-      if (hash === "") {
-        alert("로그인 성공");
-        window.location.replace("#loginmain");
-      }
-    } else {
-      if (hash !== "") {
-        alert("로그아웃 성공");
-        window.location.replace("");
-      }
-    }
-  });
-});
+// 윤숙 - 아래 주석 처리된 코드 쓰면 이 부분 지우기
+document.addEventListener("DOMContentLoaded", handleLocation);
 
-function closeTabClick() {
-  window.close();
-}
+// 윤숙 - 회원가입이 되면 쓸 코드라 잠시 주석 처리
+// document.addEventListener("DOMContentLoaded", function () {
+//   authService.onAuthStateChanged((user) => {
+//     handleLocation();
+//     const hash = window.location.hash;
+//     if (user) {
+//       if (hash === "") {
+//         alert("로그인 성공");
+//         window.location.replace("#loginmain");
+//       }
+//     } else {
+//       if (hash !== "") {
+//         alert("로그아웃 성공");
+//         window.location.replace("");
+//       }
+//     }
+//   });
+// });
+
+// function closeTabClick() {
+//   window.close();
+// }
 
 // onclick, onchange, onsubmit 이벤트 핸들러 리스트
 window.route = route;
--- js/router.js
+++ js/router.js
@@ -7,14 +7,14 @@ export const route = (event) => {
 
 // 윤숙 - 페이지 경로 설정함 / "/"이 auth.html이 아니라 index.html(우리의 메인 페이지)
 const routes = {
-  404: "/pages/404.html",
-  "/": "index.html",
+  "/": "/index.html",
   post: "/pages/post.html",
   profile: "/pages/profile.html",
   mypage: "/pages/mypage.html",
   feed: "/pages/feed.html",
   auth: "/pages/auth.html",
   loginmain: "/pages/loginmain.html",
+  404: "/pages/404.html",
 };
 
 // 윤숙 - 댓글을 다는 창이 feed.html이니까 feed.js로 수정함
@@ -23,32 +23,43 @@ const routes = {
 export const handleLocation = async () => {
   let path = window.location.hash.replace("#", "");
   const pathName = window.location.pathname;
-
-  // 윤숙 - 우리는 메인이 index.page니까 이 코드가 필요하지 않을 것 같아 일단 주석처리
-  // Live Server를 index.html에서 오픈할 경우
-  // if (pathName === "/index.html") {
-  //   window.history.pushState({}, "", "/");
-  // }
-
   if (path.length == 0) {
     path = "/";
   }
+  const route = routes[path] || routes[404]; // truthy 하면 route[path], falsy 하면 routes[404]
 
-  const route = routes[path] || routes[404];
   const html = await fetch(route).then((data) => data.text());
-  // document.getElementById("root").innerHTML = html;
-  document.getElementById("main-page").innerHTML = html;
 
-  // if (path === "loginmain") {
-  //   // 로그인한 회원의 프로필사진과 닉네임을 화면에 표시해줌.
-  //   document.getElementById("nickname").textContent =
-  //     authService.currentUser.displayName ?? "닉네임 없음";
+  document.getElementById("root").innerHTML = html;
+};
 
-  //   document.getElementById("profileImg").src =
-  //     authService.currentUser.photoURL ?? "../assets/blankProfile.webp";
+// 윤숙 - 아래 코드 쓰면 여기서부터 지우기
+
+// 윤숙 - 우리는 메인이 index.page니까 이 코드가 필요하지 않을 것 같아 일단 주석처리
+// Live Server를 index.html에서 오픈할 경우
+// if (pathName === "/index.html") {
+//   window.history.pushState({}, "", "/");
+// }
+
+// if (path.length == 0) {
+//   path = "/";
+// }
+
+// const route = routes[path] || routes[404];
+// const html = await fetch(route).then((data) => data.text());
+// document.getElementById("root").innerHTML = html;
+
+// 윤숙 - 로그인 구현 전까지 주석처리
+//   if (path === "loginmain") {
+//     // 로그인한 회원의 프로필사진과 닉네임을 화면에 표시해줌.
+//     document.getElementById("nickname").textContent =
+//       authService.currentUser.displayName ?? "닉네임 없음";
+
+//     document.getElementById("profileImg").src =
+//       authService.currentUser.photoURL ?? "../assets/blankProfile.webp";
 
 //     getCommentList();
-  // }
+//   }
 //   if (path === "profile") {
 //     document.getElementById("profileView").src =
 //       authService.currentUser.photoURL ?? "/assets/blankProfile.webp";
@@ -59,6 +70,4 @@ export const handleLocation = async () => {
 // };
 
 // export const goToProfile = () => {
-//   window.location.hash = "#profile";
-// };
-// 
+//   window.location.hash = "#profile";}
