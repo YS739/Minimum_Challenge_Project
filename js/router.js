e4fc948a76c77b8e5ccb66df062a16fd5d81291e
daeho sim <tlaeogh5@gmail.com>
2022-11-24 17:52:33 +0900

    제발되라 백업

--- js/router.js
+++ js/router.js
@@ -24,6 +24,7 @@ export const handleLocation = async () => {
   // "http://example.com/"가 아니라 도메인 뒤에 / 없이 "http://example.com" 으로 나오는 경우
   if (path.length == 0) {
     path = "/";
+    getPostList();                      //이 함수땜에 로그아웃해도 나오네요 이미지
   }
 
   const route = routes[path] || routes[404]; // truthy 하면 route[path], falsy 하면 routes[404]
