6d833c8c172e5ba631811d1f263f9d143eb3ceb7
Yuan Kim <mini5167@naver.com>
2022-11-24 21:37:15 +0900

    please

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { authService, dbService } from "./firebase.js";
-import { handleLocation, route, goToPost, goToMyPage } from "./router.js";
+import { authService } from "./firebase.js";
+import { handleLocation, route, goToPost, goToMyPage, goToHome } from "./router.js";
 // 윤숙 - 새로 만든 이벤트들 이렇게 수입(import)까지 잘 하기!
 import { socialLogin } from "./pages/auth.js";
 import { handleAuth, onToggle } from "./pages/auth.js";
@@ -7,14 +7,17 @@ import { handleAuth, onToggle } from "./pages/auth.js";
 import { changeProfile, onFileChange } from "./pages/profile.js";
 import { onPostChange, save_post } from "./pages/post.js";
 import { getPostList } from "./pages/mypage.js";
+import { getFeedCommentList,onEditing,update_comment, delete_comment, save_comment} from "./pages/feed.js";
+
 
 window.addEventListener("hashchange", handleLocation);
 
 // 첫 랜딩 또는 새로고침 시
 document.addEventListener("DOMContentLoaded", () => {
+  getPostList();
+
   authService.onAuthStateChanged((user) => {
     handleLocation();
-
     const hash = window.location.hash;
     if (user) {
       if (hash === "auth") {
@@ -27,6 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+
 
 // 전역 함수 리스트
 window.route = route;
@@ -41,9 +48,12 @@ window.onPostChange = onPostChange;
 window.save_post = save_post;
 // my page event
 window.getPostList = getPostList;
-// 유안 - 프로필 사진 변경, 마이페이지에서 작성하기링크로
+// 유안 - 프로필 사진 변경
 window.onFileChange = onFileChange;
 window.changeProfile = changeProfile;
-window.goToWrite = goToWrite;
-window.goToMyProfile = goToMyProfile;
-
+window.goToHome = goToHome;
+window.getFeedCommentList = getFeedCommentList;
+window.onEditing = onEditing;
+window.update_comment = update_comment;
+window.delete_comment = delete_comment;
+window.save_comment = save_comment;
--- js/router.js
+++ js/router.js
@@ -1,5 +1,6 @@
 import { authService } from "./firebase.js";
 import { getPostList } from "./pages/loginmain.js";
+import { getFeedCommentList } from "./pages/feed.js";
 
 export const route = (event) => {
   event.preventDefault();
@@ -17,13 +18,19 @@ const routes = {
   404: "/pages/404.html",
 };
 
+
 export const handleLocation = async () => {
+  
   let path = window.location.hash.replace("#", ""); // ""
 
   // "http://example.com/"가 아니라 도메인 뒤에 / 없이 "http://example.com" 으로 나오는 경우
   if (path.length == 0) {
     path = "/";
-    getPostList();
+    getPostList();                      //이 함수땜에 로그아웃해도 나오네요 이미지
+  }
+  if (path === "feed") {
+    
+    getFeedCommentList();                      //이 함수땜에 로그아웃해도 나오네요 이미지
   }
 
   const route = routes[path] || routes[404]; // truthy 하면 route[path], falsy 하면 routes[404]
@@ -32,16 +39,20 @@ export const handleLocation = async () => {
 
   document.getElementById("root").innerHTML = html;
 
+
+
   // 특정 화면 렌더링 되자마자 DOM 조작 처리
   if (path === "loginmain") {
     getPostList();
-
     // 로그인한 회원의 프로필사진과 닉네임을 화면에 표시해줌.
     document.getElementById("nickname").textContent =
       authService.currentUser.displayName ?? "회원";
 
     document.getElementById("profileImg").src =
       authService.currentUser.photoURL ?? "/img/강아지.jpg";
+
+    //윤숙 - 이 부분은 포스트를 불러올 거라 일단 보류
+    
   }
 
   if (path === "mypage") {
@@ -62,18 +73,19 @@ export const handleLocation = async () => {
       authService.currentUser.photoURL ?? "/img/강아지.jpg";
     document.getElementById("profileNickname").placeholder =
       authService.currentUser.displayName ?? "회원";
+
   }
 
-  const modal = document.getElementById("login-modal");
-  const btnModal = document.getElementById("loginButton");
-  btnModal.addEventListener("click", (e) => {
-    modal.style.display = "flex";
-  });
+      const modal = document.getElementById("login-modal")
+    const btnModal = document.getElementById("loginButton")
+    btnModal.addEventListener("click", e => {
+        modal.style.display = "flex"
+    })
 
-  const closeBtn = modal.querySelector("#closeBtn");
-  closeBtn.addEventListener("click", (e) => {
-    modal.style.display = "none";
-  });
+    const closeBtn = modal.querySelector("#closeBtn")
+    closeBtn.addEventListener("click", e => {
+        modal.style.display = "none"
+    })
 };
 // 윤숙 - 다른 페이지 이동할 때 이렇게 이벤트를 만들기!
 export const goToPost = () => {
@@ -82,4 +94,9 @@ export const goToPost = () => {
 
 export const goToMyPage = () => {
   window.location.hash = "#mypage";
+
+};
+
+export const goToHome = () => {
+  window.location.hash = "";
 };
