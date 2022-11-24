a489bedfd8e0cd99a7d252984fcd12055a6b32d9
daeho sim <tlaeogh5@gmail.com>
2022-11-24 14:57:50 +0900

    백업  최종본

--- js/router.js
+++ js/router.js
@@ -1,5 +1,4 @@
 import { authService } from "./firebase.js";
-import { getPostList } from "./pages/mypage.js";
 
 export const route = (event) => {
   event.preventDefault();
@@ -18,6 +17,7 @@ const routes = {
 };
 
 export const handleLocation = async () => {
+  
   let path = window.location.hash.replace("#", ""); // ""
 
   // "http://example.com/"가 아니라 도메인 뒤에 / 없이 "http://example.com" 으로 나오는 경우
@@ -32,7 +32,6 @@ export const handleLocation = async () => {
   document.getElementById("root").innerHTML = html;
 
   // 특정 화면 렌더링 되자마자 DOM 조작 처리
-
   if (path === "loginmain") {
     // 로그인한 회원의 프로필사진과 닉네임을 화면에 표시해줌.
     document.getElementById("nickname").textContent =
@@ -63,16 +62,22 @@ export const handleLocation = async () => {
       authService.currentUser.photoURL ?? "/img/강아지.jpg";
     document.getElementById("profileNickname").placeholder =
       authService.currentUser.displayName ?? "회원";
+
   }
+
+      const modal = document.getElementById("login-modal")
+    const btnModal = document.getElementById("loginButton")
+    btnModal.addEventListener("click", e => {
+        modal.style.display = "flex"
+    })
+
+    const closeBtn = modal.querySelector("#closeBtn")
+    closeBtn.addEventListener("click", e => {
+        modal.style.display = "none"
+    })
 };
 // 윤숙 - 다른 페이지 이동할 때 이렇게 이벤트를 만들기!
-export const goToMyPage = () => {
-  window.location.hash = "#mypage";
-  document.getElementById("goMYBtn").disabled = true;
-};
-
 export const goToPost = () => {
-  document.getElementById("goPoBtn").disabled = true;
   window.location.hash = "#post";
 };
 
