49ee28c8dbbf790dae3fcd5357684bd36bce5c9a
Yujin Son <y09urt77@gmail.com>
2022-11-24 23:18:16 +0900

    체크체크

--- js/pages/feed.js
+++ js/pages/feed.js
@@ -1,4 +1,4 @@
-//  댓글 부분
+// 댓글부분
 import {
   doc,
   addDoc,
@@ -40,7 +40,7 @@ export const onEditing = (event) => {
   const cardBody = event.target.parentNode.parentNode;
   const commentText = cardBody.children[0].children[0];
   const commentInputP = cardBody.children[0].children[1];
-
+  
   commentText.classList.add("noDisplay");
   commentInputP.classList.add("d-flex");
   commentInputP.classList.remove("noDisplay");
@@ -71,7 +71,7 @@ export const update_comment = async (event) => {
 export const delete_comment = async (event) => {
   event.preventDefault();
   const id = event.target.nickname;
-  const ok = window.confirm("해당 응원글을 정말 삭제하시겠습니까?");
+  const ok = window.confirm("해당 댓글을 정말 삭제하시겠습니까?");
   if (ok) {
     try {
       await deleteDoc(doc(dbService, "feedCommentList", id));
@@ -101,7 +101,6 @@ export const getFeedCommentList = async () => {
   commentList.innerHTML = "";
   cmtObjList.forEach((cmtObj) => {
     const isOwner = currentUid === cmtObj.creatorId;
-<<<<<<< HEAD
     const temp_html = `<div class="feed-commentCard">
             <div class="feed-cards">
                 <blockquote class="blockquote mb-0">
@@ -112,23 +111,10 @@ export const getFeedCommentList = async () => {
                     <footer class="feedCards-footer"><div>BY&nbsp;&nbsp;<img class="cmtImg" width="50px" height="50px" src="${
                       cmtObj.profileImg
                     }" alt="profileImg" /><span>${
-=======
-    const temp_html = `<div class="card commentCard">
-    <div class="card-body">
-        <blockquote class="blockquote mb-0">
-            <p class="commentText">${cmtObj.text}</p>
-            <p id="${
-              cmtObj.id
-            }" class="noDisplay"><input class="newCmtInput" type="text"  /><button class="updateBtn" onclick="update_comment(event)">완료</button></p>
-            <footer class="quote-footer"><div>BY&nbsp;&nbsp;<img class="cmtImg" width="50px" height="50px" src="${
-              cmtObj.profileImg
-            }" alt="profileImg" /><span>${
->>>>>>> 83f388ceef8911e3b3e5b9533b863a74f3f091ec
       cmtObj.nickname ?? "회원"
     }</span></div><div class="cmtAt">${new Date(cmtObj.createdAt)
       .toString()
       .slice(0, 25)}</div></footer>
-<<<<<<< HEAD
                 </blockquote>
                 <div class="${isOwner ? "updateBtns" : "noDisplay"}">
                      <button onclick="onEditing(event)" class="cmtEditBt">수정</button>
@@ -138,19 +124,8 @@ export const getFeedCommentList = async () => {
                 </div>            
               </div>
        </div>`;
-=======
-        </blockquote>
-        <div class="${isOwner ? "updateBtns" : "noDisplay"}">
-             <button onclick="onEditing(event)" class="cmtEditBtn">수정</button>
-          <button name="${
-            cmtObj.id
-          }" onclick="delete_comment(event)" class="cmtDelBtn">삭제</button>
-        </div>            
-      </div>
-</div>`;
->>>>>>> 83f388ceef8911e3b3e5b9533b863a74f3f091ec
     const div = document.createElement("div");
-    div.classList.add("mycards");
+    div.classList.add("feedCards");
     div.innerHTML = temp_html;
     commentList.appendChild(div);
   });
