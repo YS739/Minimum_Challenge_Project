3ca7340e221ce83ef1a31188d0c50779f3009035
YS739 <suk921@gmail.com>
2022-11-25 10:45:58 +0900

    making feed page-1 edit CSS of eidit,del btn

--- js/pages/feed.js
+++ js/pages/feed.js
@@ -11,6 +11,66 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 import { dbService, authService } from "../firebase.js";
 
+// 내가 클릭한 post 불러오기
+export const getPostList = async () => {
+  let pstObjList = [];
+  const q = query(
+    collection(dbService, "minipost"),
+    orderBy("createdAt", "desc")
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    const postObj = {
+      id: doc.id,
+      ...doc.data(),
+    };
+    pstObjList.push(postObj);
+  });
+  console.log(pstObjList);
+  const postList = document.getElementById("main-page");
+  const currentUid = authService.currentUser.uid;
+  postList.innerHTML = "";
+  pstObjList.forEach((ptObj) => {
+    // const isOwner = currentUid === ptObj.creatorId;
+    const temp_html = `<a href="#feed" onclick="route(event)" class="postingbox">
+    <div class="postPic">
+      <img
+        class="postPicImg"
+        width="100px"
+        height="100px"
+        src="${ptObj.postpic}"
+      />
+    </div>
+    <div class="contentbox">
+      <p class="postTitle">${ptObj.title}</p>
+      <p class="postContent">${ptObj.post}</p>
+      <footer class="posting-footer">
+        <div>
+          <img
+            class="myProfileImg"
+            width="50px"
+            height="50px"
+            src="${ptObj.profileImg}"
+            alt="profileImg"
+          /><span>${ptObj.nickname ?? "회원"}</span>
+        </div>
+        <div class="postAt">
+          ${new Date(ptObj.createdAt).toString().slice(0, 25)}
+        </div>
+      </footer>
+    </div>
+  </a>`;
+    const div = document.createElement("div");
+    div.classList.add("mypost");
+    div.innerHTML = temp_html;
+    postList.appendChild(div);
+  });
+};
+// 어떤 값을 초기화 해야 할지 몰라서 일단 주석 처리
+// pstObjList.value = "";
+getPostList();
+
+// 댓글 부분
 export const save_comment = async (event) => {
   event.preventDefault();
   const comment = document.getElementById("feedComment");
@@ -40,7 +100,7 @@ export const onEditing = (event) => {
   const cardBody = event.target.parentNode.parentNode;
   const commentText = cardBody.children[0].children[0];
   const commentInputP = cardBody.children[0].children[1];
-  
+
   commentText.classList.add("noDisplay");
   commentInputP.classList.add("d-flex");
   commentInputP.classList.remove("noDisplay");
