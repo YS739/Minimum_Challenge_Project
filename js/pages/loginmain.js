4c892885efb85f1d1d081ffb1c047df95b894172
YS739 <suk921@gmail.com>
2022-11-26 19:04:29 +0900

    revert before sim,yuan,yujin

--- js/pages/loginmain.js
+++ js/pages/loginmain.js
@@ -5,14 +5,7 @@ import {
   getDocs,
   where,
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-import { dbService, authService, storageService } from "../firebase.js";
-import {
-  ref,
-  uploadString,
-  getDownloadURL,
-} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
-import { updateProfile } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
-import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+import { dbService } from "../firebase.js";
 
 export const getPostList = async () => {
   let pstObjList = [];
@@ -193,49 +186,4 @@ export const getBookList = async () => {
     div.innerHTML = temp_html;
     postList.appendChild(div);
   });
-};
-
-// profile.js for modal
-
-export const changeProfile = async (event) => {
-  event.preventDefault();
-  document.getElementById("profileBtnEdit").disabled = true;
-  const imgRef = ref(
-    storageService,
-    `${authService.currentUser.uid}/${uuidv4()}`
-  );
-
-  const newNickname = document.getElementById("profileNickname").value;
-  const imgDataUrl = localStorage.getItem("imgDataUrl");
-  let downloadUrl;
-  if (imgDataUrl) {
-    const response = await uploadString(imgRef, imgDataUrl, "data_url");
-    downloadUrl = await getDownloadURL(response.ref);
-  }
-
-  await updateProfile(authService.currentUser, {
-    displayName: newNickname ? newNickname : null,
-    photoURL: downloadUrl ? downloadUrl : null,
-  })
-    .then(() => {
-      alert("프로필 수정 완료");
-      window.location.hash = "#loginmain";
-    })
-    .catch((error) => {
-      alert("프로필 수정 실패");
-      console.log("error:", error);
-    });
-};
-
-//프로필 사진을 클릭했을 때 나오는 동작
-export const onFileChange = (event) => {
-  const theFile = event.target.files[0]; // file 객체
-  const reader = new FileReader();
-  reader.readAsDataURL(theFile); // file 객체를 브라우저가 읽을 수 있는 data URL로 읽음.
-  reader.onloadend = (finishedEvent) => {
-    // 파일리더가 파일객체를 data URL로 변환 작업을 끝났을 때
-    const imgDataUrl = finishedEvent.currentTarget.result;
-    localStorage.setItem("imgDataUrl", imgDataUrl);
-    document.getElementById("profileView").src = imgDataUrl;
-  };
 };
--- js/router.js
+++ js/router.js
@@ -1,4 +1,4 @@
-import { authService } from "./firebase.js";
+import { authService, } from "./firebase.js";
 import { getPostList } from "./pages/loginmain.js";
 import { getFeedCommentList } from "./pages/community.js";
 
@@ -44,24 +44,39 @@ export const handleLocation = async () => {
     document.getElementById("profileImg").src =
       authService.currentUser.photoURL ?? "/img/강아지.jpg";
 
-    // 프로필 모달 기능 구현
-    const modalProfile = document.getElementById("profile-modal");
-    const btnModalProfile = document.getElementById("goMyBtn");
-    btnModalProfile.addEventListener("click", (f) => {
-      modalProfile.style.display = "flex";
-    });
+      document.getElementById("profileView").src =
+      authService.currentUser.photoURL ?? "/img/강아지.jpg";
+    document.getElementById("profileNickname").placeholder =
+      authService.currentUser.displayName ?? "회원";
 
-    const closeProBtn = modalProfile.querySelector("#closeProfileBtn");
-    closeProBtn.addEventListener("click", (f) => {
-      modalProfile.style.display = "none";
-    });
+      // 프로필 모달 기능 구현
+      const modalprofile = document.getElementById("profile-modal");
+      const btnModalprofile = document.getElementById("goMyBtn");
+      btnModalprofile.addEventListener("click", (f) => {
+        modalprofile.style.display = "flex";
+      });
+    
+      const closeProBtn = modalprofile.querySelector("#closeProfileBtn");
+      closeProBtn.addEventListener("click", (f) => {
+        modalprofile.style.display = "none";
+      });
 
-    modalProfile.addEventListener("click", (f) => {
-      const evTarget1 = f.target;
-      if (evTarget1.classList.contains("blackModal")) {
-        modalProfile.style.display = "none";
-      }
-    });
+      
+      modalprofile.addEventListener("click", (f) => {
+        const evTarget1 = f.target;
+        if (evTarget1.classList.contains("blackModal")) {
+          modalprofile.style.display = "none";
+        }
+      });
+  }
+
+  if (path === "community") {
+    getFeedCommentList();
+    document.getElementById("nickname").textContent =
+      authService.currentUser.displayName ?? "회원";
+
+    document.getElementById("profileImg").src =
+      authService.currentUser.photoURL ?? "/img/강아지.jpg";
   }
 
   if (path === "community") {
@@ -84,6 +99,7 @@ export const handleLocation = async () => {
       authService.currentUser.photoURL ?? "/img/강아지.jpg";
   }
 
+  
   // 로그인 모달 기능 구현 자바스크립트
   const modal = document.getElementById("login-modal");
   const btnModal = document.getElementById("loginButton");
